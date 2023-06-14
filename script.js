09fee46dac224c0a7aae3a337019b6e0c94eac12
Azazel <trueman898@gmail.com>
2023-06-14 11:48:42 +0300

    code refactiring

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const shareBtn = document.querySelector('.btn');
 const popup = document.querySelector('.popup');
 
-let showPopup = shareBtn.addEventListener('click', (e) => {
-	e.preventDefault();
+const showPopup = shareBtn.addEventListener('click', () => {
 	popup.classList.add('popup--active');
-	console.log(e.target);
 });
 
-let closePopup = document.addEventListener('click', (e) => {
+const closePopup = document.addEventListener('click', (e) => {
 	const isClosestBtn = e.target.closest('.btn');
 	const isClosestPopup = e.target.closest('.popup');
 	const isContainsActiveClass = popup.classList.contains('popup--active');
